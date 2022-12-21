e847cdf76c73ca4cf8d9095f2c3b0c34a4b00570
Donald_Trumpyyy <demaupeoucorentin@gmail.com>
2022-12-21 19:00:22 +0100

    empowering seo

--- pages/_document.js
+++ pages/_document.js
@@ -26,25 +26,25 @@ class MyDocument extends Document {
                     <meta charSet="utf-8" />
                     <link rel="icon" href="static/favicon.ico" />
                     <link rel="shortcut icon" href="/static/favicon.ico" />
-                    <meta name="theme-color" content="#000000" />
+                    <meta name="theme-color" content="#FFFFFF" />
                     <link rel="apple-touch-icon" href="/static/logo192.png" />
                     <meta name="google-site-verification" content="A8mouogszewpmVHxgnOtBBczFt2aT9Smx92xZDZS-tc" />
                     <link rel="manifest" href="/static/manifest.json" />
 
-                    <meta name="title" content="STRATUS — Expert Blockchain Agency" />
-                    <meta name="description" content="Metaverse, Blockchain and Web 3.0 solutions provider. Trusted by Metaverse GT." />
+                    <meta name="title" content="STRATUS ☁️ – Expert Blockchain Agency" />
+                    <meta name="description" content="Metaverse, Blockchain, NFT and Web 3.0 solutions provider. Translator of Solidity and Ethereum documentation. Trusted by Metaverse GT." />
 
                     <meta property="og:type" content="website" />
                     <meta property="og:url" content="https://stratusagency.io/" />
-                    <meta property="og:title" content="STRATUS — Expert Blockchain Agency" />
-                    <meta property="og:description" content="Metaverse, Blockchain and Web 3.0 solutions provider. Trusted by Metaverse GT." />
-                    <meta property="og:image" content="" />
+                    <meta property="og:title" content="STRATUS ☁️ – Expert Blockchain Agency" />
+                    <meta property="og:description" content="Metaverse, Blockchain, NFT and Web 3.0 solutions provider. Translator of Solidity and Ethereum documentation. Trusted by Metaverse GT." />
+                    <meta property="og:image" content="https://cdn.discordapp.com/attachments/793382333339271178/1055180454900285540/icon_black.jpg" />
 
                     <meta property="twitter:card" content="summary_large_image" />
                     <meta property="twitter:url" content="https://stratusagency.io/" />
-                    <meta property="twitter:title" content="STRATUS — Expert Blockchain Agency" />
-                    <meta property="twitter:description" content="Metaverse, Blockchain and Web 3.0 solutions provider. Trusted by Metaverse GT." />
-                    <meta property="twitter:image" content="" />
+                    <meta property="twitter:title" content="STRATUS ☁️ – Expert Blockchain Agency" />
+                    <meta property="twitter:description" content="Metaverse, Blockchain, NFT and Web 3.0 solutions provider. Translator of Solidity and Ethereum documentation. Trusted by Metaverse GT." />
+                    <meta property="twitter:image" content="https://cdn.discordapp.com/attachments/793382333339271178/1055180454900285540/icon_black.jpg" />
                 </Head>
 
                 <body>
