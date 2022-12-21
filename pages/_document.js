fda4481f0cfe763c1967ebf8acbf1d1a22e14280
Donald_Trumpyyy <demaupeoucorentin@gmail.com>
2022-12-21 19:04:14 +0100

    seo edit

--- pages/_document.js
+++ pages/_document.js
@@ -31,18 +31,18 @@ class MyDocument extends Document {
                     <meta name="google-site-verification" content="A8mouogszewpmVHxgnOtBBczFt2aT9Smx92xZDZS-tc" />
                     <link rel="manifest" href="/static/manifest.json" />
 
-                    <meta name="title" content="STRATUS ☁️ – Expert Blockchain Agency" />
+                    <meta name="title" content="STRATUS – Expert Blockchain Agency" />
                     <meta name="description" content="Metaverse, Blockchain, NFT and Web 3.0 solutions provider. Translator of Solidity and Ethereum documentation. Trusted by Metaverse GT." />
 
                     <meta property="og:type" content="website" />
                     <meta property="og:url" content="https://stratusagency.io/" />
-                    <meta property="og:title" content="STRATUS ☁️ – Expert Blockchain Agency" />
+                    <meta property="og:title" content="STRATUS – Expert Blockchain Agency" />
                     <meta property="og:description" content="Metaverse, Blockchain, NFT and Web 3.0 solutions provider. Translator of Solidity and Ethereum documentation. Trusted by Metaverse GT." />
                     <meta property="og:image" content="https://cdn.discordapp.com/attachments/793382333339271178/1055180454900285540/icon_black.jpg" />
 
                     <meta property="twitter:card" content="summary_large_image" />
                     <meta property="twitter:url" content="https://stratusagency.io/" />
-                    <meta property="twitter:title" content="STRATUS ☁️ – Expert Blockchain Agency" />
+                    <meta property="twitter:title" content="STRATUS – Expert Blockchain Agency" />
                     <meta property="twitter:description" content="Metaverse, Blockchain, NFT and Web 3.0 solutions provider. Translator of Solidity and Ethereum documentation. Trusted by Metaverse GT." />
                     <meta property="twitter:image" content="https://cdn.discordapp.com/attachments/793382333339271178/1055180454900285540/icon_black.jpg" />
                 </Head>
